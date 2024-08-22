d92800abbc7736eaabbce9b9cb133a71985ad09f
M-Mahad-Siddiqui <mahadsiddiqui21@gmail.com>
2024-08-22 22:19:24 +0500

    update usestates like create form task

--- useStates/src/App.jsx
+++ useStates/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useState } from 'react';
+import './App.css';
+import Form from './components/Form';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [user, setUser] = useState({
+		name: '',
+		lastName: '',
+		email: '',
+		rollNumber: '',
+		gender: ''
+	});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setUser((prevUser) => ({
+			...prevUser,
+			[name]: value
+		}));
+	};
+
+	const handleSubmit = () => {
+		console.log('User Details Submitted:', user);
+		// You can perform form submission or other actions here
+	};
+
+	return (
+		<>
+			<Form user={user} handleChange={handleChange} handleSubmit={handleSubmit} />
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ useStates/src/components/Form.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+function Form({ user, handleChange, handleSubmit }) {
+	return (
+		<div className="form">
+			<div className="container">
+				<h1>User Form</h1>
+				<label htmlFor="name">Name : </label>
+				<input type="text" name="name" id="name" value={user.name} onChange={handleChange} />
+				<label htmlFor="lastName">Last Name : </label>
+				<input type="text" name="lastName" id="lastName" value={user.lastName} onChange={handleChange} />
+				<label htmlFor="email">Email : </label>
+				<input type="email" name="email" id="email" value={user.email} onChange={handleChange} />
+				<label htmlFor="rollNumber">Roll Number :</label>
+				<input type="text" name="rollNumber" id="rollNumber" value={user.rollNumber} onChange={handleChange} />
+				<label htmlFor="gender">Gender :</label>
+				<select name="gender" id="gender" value={user.gender} onChange={handleChange}>
+					<option value="">Select Gender</option>
+					<option value="male">Male</option>
+					<option value="female">Female</option>
+					<option value="other">Other</option>
+				</select>
+				<button onClick={handleSubmit}>Submit</button>
+			</div>
+
+			<div className="display">
+				<h1>User Details</h1>
+				<span>
+					Name : <b>{user.name}</b>
+				</span>
+				<span>
+					Last Name : <b>{user.lastName}</b>
+				</span>
+				<span>
+					Email : <b>{user.email}</b>
+				</span>
+				<span>
+					Roll Number : <b>{user.rollNumber}</b>
+				</span>
+				<span>
+					Gender: <b>{user.gender}</b>
+				</span>
+			</div>
+		</div>
+	);
+}
+
+export default Form;
